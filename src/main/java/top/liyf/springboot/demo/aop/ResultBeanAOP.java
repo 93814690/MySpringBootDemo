f9ad8cd17ae5ec24d6ee6eb68b0adf8eff1a2198
yunfei <93814690@qq.com>
2018-11-06 11:30:30 +0800

    update: 对返回结果为 ResultBean 的 controller 统一处理

--- src/main/java/top/liyf/springboot/demo/aop/ResultBeanAOP.java
+++ src/main/java/top/liyf/springboot/demo/aop/ResultBeanAOP.java
@@ -6,19 +6,23 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import top.liyf.springboot.demo.beans.ResultBean;
 
 /**
  * @author liyf
- * @description
+ * @description 对返回结果为 ResultBean 的 controller 统一处理
  * @date Created in 2018\10\22
  */
+//将一个java类定义为切面类
 @Aspect
 @Component
-public class ControllerAOP {
+//标识切面的优先级.值越小，优先级越高
+@Order(-1)
+public class ResultBeanAOP {
 
-    private static final Logger logger = LoggerFactory.getLogger(ControllerAOP.class);
+    private static final Logger logger = LoggerFactory.getLogger(ResultBeanAOP.class);
 
     @Pointcut("execution(public top.liyf.springboot.demo.beans.ResultBean *(..))")
     public void pointcut() {}
@@ -30,7 +34,7 @@ public class ControllerAOP {
 
         try {
             result = (ResultBean<?>) joinPoint.proceed();
-            logger.info(joinPoint.getSignature() + ": use time:" + (System.currentTimeMillis() - startTime) + "ms");
+            logger.info(joinPoint.getSignature() + ": use time: " + (System.currentTimeMillis() - startTime) + "ms");
         } catch (Throwable throwable) {
             result = handlerException(joinPoint, throwable);
         }
