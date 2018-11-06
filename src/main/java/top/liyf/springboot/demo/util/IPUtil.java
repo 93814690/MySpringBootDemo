41fd748bbbd3add0ea940ad19ac80eb66cbb3ff9
yunfei <93814690@qq.com>
2018-11-06 14:07:11 +0800

    add: ip工具类

---
+++ src/main/java/top/liyf/springboot/demo/util/IPUtil.java
@@ -0,0 +1,40 @@
+package top.liyf.springboot.demo.util;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+/**
+ * @author liyf
+ * @description IP工具类
+ * @date Created in 2018\11\6 0006
+ */
+public class IPUtil {
+
+    /**
+     * 获取服务器IP
+     * @return String
+     */
+    public static String getServerIp() {
+        try {
+            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = allNetInterfaces.nextElement();
+                Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress ip = addresses.nextElement();
+                    if (ip instanceof Inet4Address
+                            && !ip.isLoopbackAddress() //loopback地址即本机地址，IPv4的loopback范围是127.0.0.0 ~ 127.255.255.255
+                            && !ip.getHostAddress().contains(":")) {
+                        System.out.println("本机的IP = " + ip.getHostAddress());
+                        return ip.getHostAddress();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "获取ip失败";
+    }
+}
