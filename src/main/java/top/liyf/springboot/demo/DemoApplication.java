d296131bf871dffe9706b1dd65e92fd13126befb
yunfei <93814690@qq.com>
2018-11-12 17:30:27 +0800

    add: 国际化支持

--- src/main/java/top/liyf/springboot/demo/DemoApplication.java
+++ src/main/java/top/liyf/springboot/demo/DemoApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import javax.servlet.MultipartConfigElement;
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableScheduling
@@ -18,7 +22,7 @@ public class DemoApplication {
 
     /**
      * 功能描述: 文件上传配置
-     * 
+     *
      * @param []
      * @return javax.servlet.MultipartConfigElement
      * @author liyf
@@ -37,6 +41,41 @@ public class DemoApplication {
         //factory.setLocation("路径地址");
 
         return factory.createMultipartConfig();
+
+    }
+
+    /**
+     * 功能描述: 会话区域解析器
+     * 设置完只针对当前的会话有效，session失效，还原为默认状态
+     *
+     * @param []
+     * @return org.springframework.web.servlet.LocaleResolver
+     * @author liyf
+     */
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        SessionLocaleResolver slr = new SessionLocaleResolver();
+//        //设置默认区域
+//        slr.setDefaultLocale(Locale.CHINA);
+//        return slr;
+//    }
+
+    /**
+     * 功能描述:
+     *
+     * @param []
+     * @return org.springframework.web.servlet.LocaleResolver
+     * @author liyf
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+
+        CookieLocaleResolver slr = new CookieLocaleResolver();
+        //设置默认区域
+        slr.setDefaultLocale(Locale.CHINA);
+        //设置cookie有效期
+        slr.setCookieMaxAge(3600 * 24 * 7);
+        return slr;
 
     }
 }
--- src/main/java/top/liyf/springboot/demo/web/HomeController.java
+++ src/main/java/top/liyf/springboot/demo/web/HomeController.java
@@ -1,18 +1,47 @@
 package top.liyf.springboot.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Locale;
 
 /**
- * @description
  * @author liyf
+ * @description
  * @date Created in 2018\10\22
  */
 @Controller
 public class HomeController {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @RequestMapping("/")
-    public String hello(){
+    public String hello(Model model) {
+        Locale locale = LocaleContextHolder.getLocale();
+        String msg = messageSource.getMessage("welcome", null, locale);
+        model.addAttribute("ht", msg);
         return "/index";
+    }
+
+    @RequestMapping("/changeLanguage")
+    public String changeLanguage(HttpServletRequest request, HttpServletResponse response, String lang) {
+        LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+        if ("zh".equals(lang)) {
+            localeResolver.setLocale(request, response, new Locale("zh", "CN"));
+        } else if ("en".equals(lang)) {
+            localeResolver.setLocale(request, response, new Locale("en", "US"));
+        }
+        return "redirect:/";
     }
 }
